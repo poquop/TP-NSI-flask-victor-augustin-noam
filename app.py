e285849e237678c09d8d56c488962a461089be50
poquop <victor.destribats@gmail.com>
2025-03-20 14:46:00 +0100

    m_id-ç^mi

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 
 app = Flask(__name__)
@@ -6,10 +6,14 @@ app = Flask(__name__)
 menu_links = [
     {"name": "Accueil", "url": "/"},
     {"name": "Concessions", "url": "/concessions"},
-    {"name": "À propos", "url": "/about"}
+    {"name": "À propos", "url": "/about"},
+    {"name": "rajouter un lien", "url" : "/ajoutage"}
 ]
 
-
+links = []
+@app.route('/ajoutage')
+def ajoutage():
+    return render_template('page_pour_ajouter', menu_links[3])
 
 @app.route('/')
 def index():
@@ -24,5 +28,37 @@ def concessions():
 def about():
     return render_template('about.html', menu_links=menu_links)
 
+
+@app.route('/')
+def index2():
+    return render_template('index.html')
+
+@app.route('/links', methods=['GET'])
+def get_links():
+    return jsonify(links)
+
+@app.route('/add_link', methods=['POST'])
+def add_link():
+    data = request.json
+    links.append({'name': data['name'], 'url': data['url']})
+    return jsonify({'message': 'Lien ajouté', 'links': links}), 201
+
+@app.route('/edit_link/<int:index>', methods=['PUT'])
+def edit_link(index):
+    if 0 <= index < len(links):
+        data = request.json
+        links[index] = {'name': data['name'], 'url': data['url']}
+        return jsonify({'message': 'Lien modifié', 'links': links}), 200
+    return jsonify({'error': 'Index invalide'}), 400
+
+@app.route('/delete_link/<int:index>', methods=['DELETE'])
+def delete_link(index):
+    if 0 <= index < len(links):
+        del links[index]
+        return jsonify({'message': 'Lien supprimé', 'links': links}), 200
+    return jsonify({'error': 'Index invalide'}), 400
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
