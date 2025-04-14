01a90702de7ca38c7689b5b2ae355307697a67b6
Jesus1sKing <jessica.thenot77@gmail.com>
2025-04-14 19:08:43 +0200

    Update createDB.sql

--- data/createDB.sql
+++ data/createDB.sql
@@ -33,5 +33,32 @@ INSERT INTO Marque(Nom, Pays) VALUES
 ;
 
 INSERT INTO Modele(Nom, idMarque) VALUES
-('', '')
+('Bugatti Veyron 16.4', '3'),
+('Bugatti Chiron', '3'),
+('BMW M4 Competition', '10'),
+('BMW M6 Coupé', '10'),
+('Corvette Z06', '4'),
+('Corvette C6', '4'),
+('McLaren 765LT Spider', '5'),
+('Mclaren 620R', '5'),
+('Lamborghini Urus', '11'),
+('Lamborghini Gallardo Superleggera', '11'),
+('Mercedes GLE', '9'),
+('Mercedes classe C', '9'), 
+('Porshe 911', '8'),
+('Porshe Taycan', '8'),
+('Nissan 370Z', '13'),
+('Nissan Juke', '13'),
+('Toyota RAV4', '12'),
+('Toyota Corolla', '12'),
+('Alfa Romeo Giulia', '1'),
+('Alfa Romeo Stelvio', '1'),
+('Ferrari SP48 Unica', '7'),
+('Ferrari SF90 Stradale', '7'),
+('Aston Martin DB11', '2'), 
+('Aston Martin Vulcan', '2'),
+('Pagani Huayra Roadster BC', '6'),
+('Pagani Zonda', '6'),
+('Koenigsegg Jesko Absolut', '14'),
+('Koenigsegg Gemera', '14')
 ;
